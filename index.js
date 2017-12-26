8b2b666510d407d4685215309c50932fd30aaefa
Ray Zane <ray@promptworks.com>
2017-12-26 16:32:41 -0500

    The ciscospark JS sdk relies on process.nextTick

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 
+/**
+ * CiscoSpark relies on `nextTick`, which apparently
+ * isn't available in React Native.
+ */
+process.nextTick = setImmediate;
+
 AppRegistry.registerComponent('SparkGoggles', () => App)
