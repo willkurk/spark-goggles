d3d219fc375dc23bc0de6ca895fec0327fb21cbd
Ray Zane <ray@promptworks.com>
2017-12-26 14:02:06 -0500

    Delete references to trash

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
 import { View, StatusBar } from 'react-native'
 import ReduxNavigation from '../Navigation/ReduxNavigation'
-import { connect } from 'react-redux'
-import StartupActions from '../Redux/StartupRedux'
 
 // Styles
 import styles from './Styles/RootContainerStyles'
 
 class RootContainer extends Component {
-  componentDidMount () {
-    this.props.startup()
-  }
-
   render () {
     return (
       <View style={styles.applicationView}>
@@ -22,9 +16,4 @@ class RootContainer extends Component {
   }
 }
 
-// wraps dispatch to create nicer functions to call within our component
-const mapDispatchToProps = (dispatch) => ({
-  startup: () => dispatch(StartupActions.startup())
-})
-
-export default connect(null, mapDispatchToProps)(RootContainer)
+export default RootContainer
