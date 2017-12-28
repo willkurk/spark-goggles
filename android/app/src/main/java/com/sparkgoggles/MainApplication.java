502abd7296ff36456fff78e737f033c76e6fb7e5
Ray Zane <ray@promptworks.com>
2017-12-28 13:21:31 -0500

    Remove react-native-webrtc

--- android/app/src/main/java/com/sparkgoggles/MainApplication.java
+++ android/app/src/main/java/com/sparkgoggles/MainApplication.java
@@ -3,7 +3,6 @@ package com.sparkgoggles;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oney.WebRTCModule.WebRTCModulePackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new WebRTCModulePackage(),
             new ReactNativeConfigPackage(),
             new ReactNativeSpark()
       );
