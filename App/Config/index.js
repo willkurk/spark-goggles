e89cfc095f05cf2ca1e820f7ff3d45ff9f62beb5
Ray Zane <ray@promptworks.com>
2018-01-03 12:09:24 -0500

    Fix __DEV__ global

--- App/Config/index.js
+++ App/Config/index.js
@@ -8,5 +8,5 @@ Text.defaultProps.allowFontScaling = AppConfig.allowTextFontScaling;
 if (__DEV__) {
   // If ReactNative's yellow box warnings are too much, it is possible to turn
   // it off, but the healthier approach is to fix the warnings.  =)
-  console.disableYellowBox = !DebugConfig.yellowBox;
+  console.disableYellowBox = !DebugConfig.yellowBox; // eslint-disable-line
 }
