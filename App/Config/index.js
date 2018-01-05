b55b263c619e502932c9f8c9a860819b83e5bfb0
Ray Zane <ray@promptworks.com>
2018-01-05 14:07:52 -0500

    Ignore warning about Remote debugger

--- App/Config/index.js
+++ App/Config/index.js
@@ -9,4 +9,5 @@ if (__DEV__) {
   // If ReactNative's yellow box warnings are too much, it is possible to turn
   // it off, but the healthier approach is to fix the warnings.  =)
   console.disableYellowBox = !DebugConfig.yellowBox; // eslint-disable-line
+  console.ignoredYellowBox = ['Remote debugger']; // eslint-disable-line
 }
