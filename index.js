a3035fee402e7ae6a239335eb9f1eb1451900781
Tom Chandler <thomas@promptworks.com>
2018-01-11 16:03:39 -0500

    Remove unecessary monkeypatch

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ import './App/Config/ReactotronConfig';
 import { AppRegistry } from 'react-native';
 import App from './App/Containers/App';
 
-/**
- * CiscoSpark relies on `nextTick`, which apparently
- * isn't available in React Native.
- */
-process.nextTick = setImmediate;
-
 AppRegistry.registerComponent('SparkGoggles', () => App);
