35c2fd36014619dedca07dbc66e7ddd136ebf347
Ray Zane <ray@promptworks.com>
2018-01-11 14:30:43 -0500

    We dont actually need to see the local view

--- App/Containers/Styles/MainStyles.js
+++ App/Containers/Styles/MainStyles.js
@@ -4,10 +4,12 @@ import { ApplicationStyles } from '../../Themes/';
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
 
+  // By setting the size to 1x1, you won't really see the local view, but
+  // it will still be there. Display none will not work here, because it
+  // will actually remove the video view.
   localView: {
-    // width: 10,
-    // height: 10
-    flex: 1
+    width: 1,
+    height: 1
   },
 
   remoteView: {
