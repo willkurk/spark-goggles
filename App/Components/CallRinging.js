c4f2b6ec66af67a285c2e28bb9e4a221d84d6e55
Ray Zane <ray@promptworks.com>
2018-01-15 13:45:39 -0500

    So beautiful, like the work of Leonardo DaVinci

--- App/Components/CallRinging.js
+++ App/Components/CallRinging.js
@@ -15,10 +15,19 @@ const CallRinging = ({ call, onAccept, onReject, onHangup }) => {
   }
 
   return (
-    <View style={{ flex: 1 }}>
-      <Loading text={`${call.person.email} is calling...`} />
-      <Button title="Answer" onPress={onAccept} />
-      <Button title="Reject" onPress={onReject} />
+    <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+      <View style={{ flex: 1 }}>
+        <Loading text={`${call.person.email} is calling...`} />
+      </View>
+
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ flex: 0.5 }}>
+          <Button title="Answer" onPress={onAccept} color="green" />
+        </View>
+        <View style={{ flex: 0.5 }}>
+          <Button title="Reject" onPress={onReject} color="red" />
+        </View>
+      </View>
     </View>
   );
 };
