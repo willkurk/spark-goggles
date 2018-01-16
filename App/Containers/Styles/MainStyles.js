1ee79a8388880158fb6e82bf9abc1df66f8cac16
Andrew Croce <andrew.croce@gmail.com>
2018-01-16 17:08:08 -0500

    Position remote view on top of localview background Style buttons to overlay

--- App/Containers/Styles/MainStyles.js
+++ App/Containers/Styles/MainStyles.js
@@ -1,5 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import { ApplicationStyles } from '../../Themes/';
+
+const { width, height } = Dimensions.get('window');
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
@@ -7,12 +9,44 @@ export default StyleSheet.create({
   // By setting the size to 1x1, you won't really see the local view, but
   // it will still be there. Display none will not work here, because it
   // will actually remove the video view.
+
+  callViewHidden: {
+    display: 'none'
+  },
+  callViewVisible: {
+    width: width,
+    height: height
+  },
   localView: {
-    width: 1,
-    height: 1
+    position: 'absolute',
+    width: width,
+    height: height,
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
   },
 
   remoteView: {
-    flex: 1
+    width: 50,
+    height: 50,
+    top: 0,
+    right: 0
+  },
+
+  callRinging: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: width
+  },
+
+  callConnected: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: width
   }
 });
