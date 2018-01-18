045cf9958fa4bd220db3bd260866de8a51771dad
Andrew Croce <andrew.croce@gmail.com>
2018-01-18 15:13:04 -0500

    add green and red

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -4,7 +4,9 @@ const colors = {
   lightGrey: '#d9d9d9',
   paleGrey: '#f5f5f5',
   clear: 'transparent',
-  teal: '#0bcce8'
+  teal: '#0bcce8',
+  green: '#32c655',
+  red: '#e74a3e'
 };
 
 export default colors;
