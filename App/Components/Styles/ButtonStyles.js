5f5a7801400d6e00625ba337f361b215e5e6345c
Andrew Croce <andrew.croce@gmail.com>
2018-01-18 15:12:11 -0500

    custom button

---
+++ App/Components/Styles/ButtonStyles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+import { Colors, Type, Metrics } from '../../Themes';
+
+export default StyleSheet.create({
+  button: {
+    padding: Metrics.gap(2),
+    borderRadius: Metrics.gap(4),
+    flexDirection: 'row'
+  },
+  defaultType: {
+    backgroundColor: Colors.teal
+  },
+  successType: {
+    backgroundColor: Colors.green
+  },
+  warnType: {
+    backgroundColor: Colors.red
+  },
+  label: {
+    ...Type.default,
+    color: Colors.white,
+    paddingHorizontal: Metrics.gap()
+  },
+  icon: {
+    color: Colors.white,
+    paddingHorizontal: Metrics.gap(0.5)
+  }
+});
--- App/Containers/Login.js
+++ App/Containers/Login.js
@@ -6,6 +6,7 @@ import { authenticate } from '../Redux/Login';
 import OAuth from '../Services/OAuth';
 import Loading from '../Components/Loading';
 import Error from '../Components/Error';
+import Button from '../Components/Button';
 import styles from './Styles/LoginStyles';
 
 const mapStateToProps = state => state.login;
@@ -24,6 +25,14 @@ class Login extends Component {
     if (this.props.error) {
       return <Error text={this.props.error} />;
     }
+      return (
+        <Button
+          label="Login to Spark"
+          icon="ios-unlock-outline"
+          onPress={() => OAuth.redirect()}
+        />
+      );
+    };
 
     return (
       <View style={styles.container}>
