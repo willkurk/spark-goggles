5cd688aff895a663d6c6f19486f44b923c65d8aa
Andrew Croce <andrew.croce@gmail.com>
2018-01-18 15:12:36 -0500

    Style loading indicator

--- App/Components/Loading.js
+++ App/Components/Loading.js
@@ -4,9 +4,9 @@ import { View, ActivityIndicator, Text } from 'react-native';
 import styles, { spinnerColor } from './Styles/LoadingStyles';
 
 const Loading = ({ text }) => (
-  <View style={styles.container}>
+  <View>
     <ActivityIndicator size="large" color={spinnerColor} />
-    {text && <Text style={styles.text}>{text}</Text>}
+    {text && <Text style={styles.loadingText}>{text}</Text>}
   </View>
 );
 
