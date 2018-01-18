be7f4a99eebc1f0fbd8be56ccc1a6b2428d9c696
Andrew Croce <andrew.croce@gmail.com>
2018-01-18 16:37:00 -0500

    clean up

--- App/Containers/Styles/MainStyles.js
+++ App/Containers/Styles/MainStyles.js
@@ -29,12 +29,9 @@ export default StyleSheet.create({
     position: 'absolute',
     width: width,
     height: height,
-    //left: 0,
     right: 0,
     top: 0,
-    //bottom: 0,
-    elevation: 3,
-    flex: 1
+    elevation: 3
   },
 
   callRinging: {
