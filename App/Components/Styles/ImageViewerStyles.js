28c29d83f3eacb78150587192e5f9dd39d4e1da1
Andrew Croce <andrew.croce@gmail.com>
2018-01-24 12:10:41 -0500

    Image viewer styles

--- App/Components/Styles/ImageViewerStyles.js
+++ App/Components/Styles/ImageViewerStyles.js
@@ -1,16 +1,39 @@
 import { StyleSheet } from 'react-native';
+import { Metrics } from '../../Themes';
 
 export default StyleSheet.create({
   imageViewerContainer: {
-    //
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: Metrics.screenWidth,
+    height: Metrics.screenHeight,
+    elevation: 2
   },
   imageViewerImage: {
-    //
+    position: 'absolute',
+    left: 0,
+    top: 0
+  },
+  imageReducedSizeStyle: {
+    width: 160,
+    height: Metrics.screenHeight
+  },
+  imageFullSizeStyle: {
+    width: Metrics.screenWidth,
+    height: Metrics.screenHeight
   },
   imageViewerButtons: {
-    //
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: Metrics.screenWidth,
+    margin: Metrics.gap(),
+    flexDirection: 'row',
+    justifyContent: 'center',
+    opacity: 0.5
   },
   imageViewerButton: {
-    //
+    marginRight: Metrics.gap()
   }
 });
